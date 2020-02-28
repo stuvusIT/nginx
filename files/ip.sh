3ff7f00e1f5aaf1ba0d463ce745a4af0229ea2c7
Adriaan Nieß <908128+AdriaanNiess@users.noreply.github.com>
2020-02-28 17:06:39 +0100

    Add flush option

--- files/ip.sh
+++ files/ip.sh
@@ -3,18 +3,23 @@
 # Usage:
 # (1) ./ip ban <ip-address>
 # (2) ./ip unban <ip-address>
+# (3) ./ip flush
 
 if [ $# -eq 0 ]; then
     echo "No arguments supplied"
     exit
 fi
 
-# Remove IP address
-sed -i "/deny $2;/d" /etc/nginx/conf.d/deny.conf
-
-# Add IP address if needed
 if [ $1 == "ban" ]; then
+    # Add address
+    sed -i "/deny $2;/d" /etc/nginx/conf.d/deny.conf
     echo "deny $2;" >> /etc/nginx/conf.d/deny.conf
+elif [ $1 == "unban" ]; then
+    # Remove address
+    sed -i "/deny $2;/d" /etc/nginx/conf.d/deny.conf
+elif [ $1 == "flush" ]; then
+    # Remove all addresses
+    sed -i "/^deny/d" /etc/nginx/conf.d/deny.conf
 fi
 
 # Reload nginx
