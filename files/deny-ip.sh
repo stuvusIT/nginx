aec953afc3a2181918692fb8052a211fb7519e53
Adriaan Nieß <908128+AdriaanNiess@users.noreply.github.com>
2020-03-01 14:55:57 +0100

    Improve deny-ip script

--- files/deny-ip.sh
+++ files/deny-ip.sh
@@ -1,23 +1,24 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Usage:
 # (1) ./ip ban <ip-address>
 # (2) ./ip unban <ip-address>
 # (3) ./ip flush
 
-if [ $# -eq 0 ]; then
+set -euo pipefail
+
+if [[ $# -eq 0 ]]; then
     echo "No arguments supplied"
-    exit
+    exit 1
 fi
 
-if [ $1 == "ban" ]; then
+if [[ "$1" == "ban" ]] && ! grep -q "deny $2;" /etc/nginx/conf.d/deny.conf; then
     # Add address
-    sed -i "/deny $2;/d" /etc/nginx/conf.d/deny.conf
     echo "deny $2;" >> /etc/nginx/conf.d/deny.conf
-elif [ $1 == "unban" ]; then
+elif [[ "$1" == "unban" ]]; then
     # Remove address
     sed -i "/deny $2;/d" /etc/nginx/conf.d/deny.conf
-elif [ $1 == "flush" ]; then
+elif [[ "$1" == "flush" ]]; then
     # Remove all addresses
     sed -i "/^deny/d" /etc/nginx/conf.d/deny.conf
 fi
